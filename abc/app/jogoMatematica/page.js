6583234b4a5d6ef1dadba116e60092bb30ffd7cc
Maria <110576299+md-sart@users.noreply.github.com>
2025-06-11 04:33:00 +0000

    .

--- abc/app/jogoMatematica/page.js
+++ abc/app/jogoMatematica/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Header from "../components/Header";
 
 export default function JogoMatematica() {
   const [pergunta, setPergunta] = useState(gerarPergunta());
--- abc/app/jogoPalavras/page.js
+++ abc/app/jogoPalavras/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Header from "../components/Header";
 
 const palavras = ["computador", "educacao", "brasil", "banana", "javascript"];
 
